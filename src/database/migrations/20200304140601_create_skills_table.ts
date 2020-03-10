840009c742833f53429a4cf33b08a3a38c8c7d1c
aleksandr <aleksandr.drobot@nixsolutions.com>
2020-03-10 13:09:42 +0200

    Add skills table migrations

--- src/database/migrations/20200304140601_create_skills_table.ts
+++ src/database/migrations/20200304140601_create_skills_table.ts
@@ -2,8 +2,8 @@ import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<any> {
   return knex.schema.createTable('skills', (table) => {
-    table.string('id').notNullable();
-    table.string('name', 50).notNullable();
+    table.increments('id').notNullable();
+    table.string('name', 50).unique().notNullable();
     table.timestamps(true, true);
   });
 }
