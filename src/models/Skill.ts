f1e5aec5cce936a84e94b07f9d3d2d63d44fea0b
aleksandr <aleksandr.drobot@nixsolutions.com>
2020-03-10 13:09:59 +0200

    Add skill model

---
+++ src/models/Skill.ts
@@ -0,0 +1,27 @@
+import BaseModel from './BaseModel';
+
+class Skill extends BaseModel {
+  id!: string;
+  name!: string;
+  created_at?: string;
+  updated_at?: string;
+
+  static tableName = 'skills';
+  static hidden = ['created_at', 'updated_at'];
+
+  static jsonSchema = {
+    type: 'object',
+    properties: {
+      id: { type: 'uuid' },
+      name: { type: 'string', minLength: 1, maxLength: 50 },
+      created_at: { type: 'datetime' },
+      updated_at: { type: 'datetime' },
+    },
+  };
+
+  $beforeUpdate() {
+    this.updated_at = new Date().toISOString();
+  }
+}
+
+export default Skill;
